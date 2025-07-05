874b177545400148caa4d140ddcae6cdb3be8edb
E-ADEOYE <adeoyeemmanuel996@gmail.com>
2025-07-05 19:24:15 -0400

    This is the beginning of Vue.js!

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+console.log("Let's get started with Vue.js!");
+
+const app = Vue.createApp({
+    data(){
+        return{
+            greeting: 'Welcome to the Vue.js!',
+            message: 'This message is displayed based on a boolean property.',
+            showMessage: true
+
+        };
+    }
+
+});
+
+app.mount('#app');
